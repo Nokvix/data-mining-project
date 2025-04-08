bd030e4929f1fd416fb693e65ce0a536f80d6d20
Nokvix <igor.gass2003@mail.ru>
2025-04-08 20:29:50 +0500

    Вытащил ссылки на карточки и записал в отдельный файл

---
+++ data_processing/get_url_to_positions.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+from typing import List
+
+
+def write_urls_to_file(urls: List[str]) -> None:
+    with open('urls.txt', 'a', encoding='utf-8') as file:
+        for url in urls:
+            file.write(url + '\n')
+
+
+def get_url() -> None:
+    for page in range(1, 100):
+        with open(f'pages/page_{page}.html', 'r', encoding='utf-8') as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, 'lxml')
+
+        cards = soup.find_all(class_='ListingItemTitle__link')
+        urls = [block.get('href') for block in cards]
+        write_urls_to_file(urls)
+        print(f'{page}/99')
+
+
+def main():
+    get_url()
+
+
+if __name__ == '__main__':
+    main()
